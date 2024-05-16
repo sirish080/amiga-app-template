a00ec06e94c7352ff2fe9cd2939b5badb5e36472
Ian B <ian@tesseraktinstitute.org>
2024-05-16 22:59:46 +0000

    test. see if exit button returns us to launcher

--- ts/src/components/App.tsx
+++ ts/src/components/App.tsx
@@ -1,14 +1,27 @@
-import TopicMonitor from './TopicMonitor';
+import TopicMonitor from "./TopicMonitor";
 
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <h1>Farm-ng Monitor</h1>
-                <TopicMonitor />
-            </header>
-        </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Farm-ng Monitor</h1>
+        <TopicMonitor />
+      </header>
+      <button
+        style={{
+          position: "absolute",
+          bottom: 5,
+          left: 10,
+          zIndex: 1000,
+        }}
+        onClick={
+            (() =>  window.location.href = `${window.location.protocol}//${window.location.hostname}/apps/launcher`)
+        }
+      >
+        EXIT
+      </button>
+    </div>
+  );
 }
 
 export default App;
