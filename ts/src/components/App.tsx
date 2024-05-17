07cd97ad411988ca469dd37fa57f65ddb68c5c26
Gui <gui@farm-ng.com>
2024-05-17 22:16:17 +0000

    Separte Exit button and add refresh logic to topic monitor

--- ts/src/components/App.tsx
+++ ts/src/components/App.tsx
@@ -1,4 +1,5 @@
 import TopicMonitor from "./TopicMonitor";
+import ExitButton from "./ExitButton";
 
 function App() {
   return (
@@ -7,19 +8,7 @@ function App() {
         <h1>Farm-ng Monitor</h1>
         <TopicMonitor />
       </header>
-      <button
-        style={{
-          position: "absolute",
-          bottom: 5,
-          left: 10,
-          zIndex: 1000,
-        }}
-        onClick={
-            (() =>  window.location.href = `${window.location.protocol}//${window.location.hostname}/apps/launcher`)
-        }
-      >
-        EXIT
-      </button>
+      <ExitButton />
     </div>
   );
 }
--- ts/src/components/TopicMonitor.tsx
+++ ts/src/components/TopicMonitor.tsx
@@ -6,29 +6,39 @@ function TopicMonitor() {
     const [uris, setUris] = useState<string[]>([]);
     const [selectedUri, setSelectedUri] = useState<string>('');
     const [details, setDetails] = useState<any>(null);
+    const pollInterval = 2000; // Interval in milliseconds to poll for data
+
+    const fetchData = async () => {
+        try {
+            // Replace with your backend URL
+            const response = await fetch(
+                `${window.location.protocol}//${window.location.hostname}:8042/list_uris`
+            );
+            // Check if the request was successful
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+            const rawData = await response.json();
+            return Object.keys(rawData);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+            return [];
+        }
+    };
 
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                // Replace with your backend URL
-                const response = await fetch(
-                    `${window.location.protocol}//${window.location.hostname}:8042/list_uris`
-                );
-                // Check if the request was successful
-                if (!response.ok) {
-                    throw new Error('Network response was not ok ' + response.statusText);
-                }
-                const rawData = await response.json();
-                setUris(Object.keys(rawData));
-                // Select the first URI by default
-                setSelectedUri(Object.keys(rawData)[0]);
-            } catch (error) {
-                console.error('Error fetching data:', error);
+        const pollData = async () => {
+            let urisList = await fetchData();
+            while (urisList.length === 0) {
+                await new Promise(resolve => setTimeout(resolve, pollInterval));
+                urisList = await fetchData();
             }
+            setUris(urisList);
+            setSelectedUri(urisList[0]);
         };
 
-        fetchData();
-      }, []);
+        pollData();
+    }, []);
 
     useEffect(() => {
         if (!selectedUri) return;
